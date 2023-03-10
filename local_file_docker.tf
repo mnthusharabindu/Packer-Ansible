9defabbe03606a766eda471d03e75b11f8f715e6
mnthusharabindu <117271589+mnthusharabindu@users.noreply.github.com>
2023-03-10 13:38:47 +0000

    added docker swarm code

--- local_file_docker.tf
+++ local_file_docker.tf
@@ -1,7 +1,6 @@
 resource "local_file" "docker-inventory-file" {
   content = templatefile("docker_details.tpl",
     {
-
       testserver01 = aws_instance.web-1.public_ip
       testserver02 = aws_instance.web-2.public_ip
       testserver03 = aws_instance.web-3.public_ip
